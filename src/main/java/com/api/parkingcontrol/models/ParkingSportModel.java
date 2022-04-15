91a7717713fbaaa766a6f89f36260ac7d1515ad8
Ulisses Mesquita <souza.mesquita@gmail.com>
2022-04-15 09:36:07 -0300

    Primeiro Commit - Criando as Tabelas do Banco @Entity

---
+++ src/main/java/com/api/parkingcontrol/models/ParkingSportModel.java
@@ -0,0 +1,14 @@
+package com.api.parkingcontrol.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "TB_PARKING_SPOT")
+public class ParkingSportModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+
+}
